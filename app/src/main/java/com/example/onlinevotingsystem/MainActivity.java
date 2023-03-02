f3d41aa91e8443f7a3d20450f8b213bca63a1ac4
Sam Elgert <elgert.sam@gmail.com>
2023-03-02 13:44:14 -0800

    Add data to Real Time DB button test

--- app/src/main/java/com/example/onlinevotingsystem/MainActivity.java
+++ app/src/main/java/com/example/onlinevotingsystem/MainActivity.java
@@ -10,11 +10,15 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-    Button logoutButton;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    Button logoutButton, addDataButton;
     TextView userName;
     FirebaseUser currUser;
 
@@ -23,8 +27,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
         auth = FirebaseAuth.getInstance();
         logoutButton = findViewById(R.id.logout_button);
+        addDataButton = findViewById(R.id.write_data);
         userName = findViewById(R.id.user_name_text);
         currUser = auth.getCurrentUser();
 
@@ -37,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
         else{
             userName.setText(currUser.getEmail());
         }
+
+        addDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Write a message to the database
+                myRef.setValue("Hello, World!");
+            }
+        });
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
