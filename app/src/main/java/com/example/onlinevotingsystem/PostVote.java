12cede346ea4b8d10818a82cc73835ec03638cdb
almatbolatbekov <almat.bolatbek22@gmail.com>
2023-03-23 12:27:39 -0700

    Added the path for the ListView items in the main actovity, once the object is pressed the post vote page comes and shows more detailed information for this post, including the voting option

---
+++ app/src/main/java/com/example/onlinevotingsystem/PostVote.java
@@ -0,0 +1,14 @@
+package com.example.onlinevotingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostVote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_vote);
+    }
+}
